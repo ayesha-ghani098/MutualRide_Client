d12977fa377f82de2aec907c119605038b47f530
AsfarHorani <asfarh281@gmail.com>
2023-02-25 20:47:46 +0500

    Finally worked

--- src/screens/Form/AddRideForm/googleMap.js
+++ src/screens/Form/AddRideForm/googleMap.js
@@ -11,31 +11,56 @@ const containerStyle = {
   height: "100vh",
 };
 
-const center = {
-  lat: 37.7749,
-  lng: -122.4194,
-};
+
 const libraries=["places"]
 const Map = () => {
   const [source, setSource] = useState("");
   const [destination, setDestination] = useState("");
-  const [searchBox, setSearchBox] = useState(null);
+  const [sourceSearchBox, setSourceSearchBox] = useState(null);
+  const [destinationSearchBox, setdestinationSearchBox] = useState(null);
 
-  const onPlacesChanged = () => {
-    if(!searchBox){
+  const [center,setCenter] = useState({
+    lat: 37.7749,
+    lng: -122.4194,
+  });
+  const onSourceChanged = () => {
+
+    if(!sourceSearchBox){
       console.error('Search boxes not initialized');
     return
     }
-    const places = searchBox.getPlaces();
+    const places = sourceSearchBox.getPlaces();
+    console.log(places)
     if (places.length > 0) {
-      setSource(places[0].name);
+      console.log(places[0].geometry)
+      setSource(places[0].geometry.location);
+      setCenter(places[0].geometry.location)
     }
   };
 
-  const onLoad = (ref) => {
-    setSearchBox(ref)
+  const onSourceLoad = (ref) => {
+    setSourceSearchBox(ref)
   };
 
+
+  const onDestinationChanged = () => {
+
+    if(!destinationSearchBox){
+      console.error('Search boxes not initialized');
+    return
+    }
+    const places = destinationSearchBox.getPlaces();
+    console.log(places)
+    if (places.length > 0) {
+      console.log(places[0].geometry)
+      setDestination(places[0].geometry.location);
+      setCenter(places[0].geometry.location)
+    }
+  };
+
+  const onDestinationLoad = (ref) => {
+    setdestinationSearchBox(ref)
+  };
 
   return (
     <LoadScript
@@ -43,32 +68,60 @@ const Map = () => {
       libraries={libraries}
     >
       <GoogleMap mapContainerStyle={containerStyle} center={center} zoom={13}>
-        {source && <Marker position={center} />}
+        {source && <Marker position={source} />}
+        {destination && <Marker position={destination} />}
+
         <>
         <StandaloneSearchBox
-          onLoad={onLoad}
-          onPlacesChanged={onPlacesChanged}
-        >
-          <input
-            type="text"
-            placeholder="Enter source location"
-            style={{
-              boxSizing: `border-box`,
-              border: `1px solid transparent`,
-              width: `240px`,
-              height: `32px`,
-              padding: `0 12px`,
-              borderRadius: `3px`,
-              boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-              fontSize: `14px`,
-              outline: `none`,
-              textOverflow: `ellipses`,
-              position: "absolute",
-              left: "50%",
-              marginLeft: "-120px",
-            }}
-          />
-        </StandaloneSearchBox>
+      onLoad={onSourceLoad}
+      onPlacesChanged={onSourceChanged}
+      style={{ marginBottom: '10px' }}
+    >
+      <input
+        type="text"
+        placeholder="Enter source location"
+        style={{
+          boxSizing: 'border-box',
+          border: '1px solid transparent',
+          width: '240px',
+          height: '32px',
+          padding: '0 12px',
+          borderRadius: '3px',
+          boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)',
+          fontSize: '14px',
+          outline: 'none',
+          textOverflow: 'ellipses',
+          position: 'relative',
+          left: '50%',
+          transform: 'translateX(-50%)',
+        }}
+      />
+    </StandaloneSearchBox>
+    <StandaloneSearchBox
+       onLoad={onDestinationLoad}
+       onPlacesChanged={onDestinationChanged}
+      style={{ marginTop: '10px' }}
+    >
+      <input
+        type="text"
+        placeholder="Enter destination location"
+        style={{
+          boxSizing: 'border-box',
+          border: '1px solid transparent',
+          width: '240px',
+          height: '32px',
+          padding: '0 12px',
+          borderRadius: '3px',
+          boxShadow: '0 2px 6px rgba(0, 0, 0, 0.3)',
+          fontSize: '14px',
+          outline: 'none',
+          textOverflow: 'ellipses',
+          position: 'relative',
+          left: '50%',
+          transform: 'translateX(-50%)',
+        }}
+      />
+    </StandaloneSearchBox>
         </>
       </GoogleMap>
     </LoadScript>
