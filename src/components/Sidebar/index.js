9f6c7b7d1b5d547b652f04cd4ee3e180715accec
ayesha-ghani098 <ayeshaghanichi@gmail.com>
2023-02-25 19:10:03 +0500

    removed request ride from sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -17,12 +17,14 @@ const Sidebar = ({ show, handleClose, type }) => {
         {
           type === "Driver"?
           <><Link className={styles.link} to="">Home</Link>
+          <Link className={styles.link} to="">My Profile</Link>
           <Link  className={styles.link} to="/driver/my-rides">My Rides</Link>
           <Link  className={styles.link} to="/driver/add-ride">Add a Ride</Link></>
           : 
           <><Link  className={styles.link} to="">Home</Link>
+          <Link className={styles.link} to="">My Profile</Link>
           <Link  className={styles.link} to="/passenger/my-rides">My Rides</Link>
-          <Link  className={styles.link} to="/passender/request-ride">Request a Ride</Link></>
+          </>
         }
  
       </Offcanvas.Body>
