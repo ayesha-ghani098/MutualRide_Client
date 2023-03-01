743dca1a9e5831838d1be9fb2710607e2fb7df5a
ayesha-ghani098 <ayeshaghanichi@gmail.com>
2023-03-01 19:23:09 +0500

    tracking component updated

--- src/screens/Tracking/Driver/index.js
+++ src/screens/Tracking/Driver/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const DriverTracking = () => {
+  const [status, setStatus] = useState("");
   return (
     <div>
-        <button>Complete</button>
+      {status !== "" && <h1>status</h1>}
+      {/* button change krny hun tu krdena style */}
+      <button>Complete</button>
     </div>
-  )
-}
+  );
+};
 
-export default DriverTracking
+export default DriverTracking;
--- src/screens/Tracking/Passenger/index.js
+++ src/screens/Tracking/Passenger/index.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from "react";
 
 const PassengerTracking = () => {
+  const [status, setStatus] = useState("");
   return (
     <div>
-        <button>Complete</button>
+      {status !== "" && <h1>status</h1>}
+      {/* button change krny hun tu krdena style */}
+      <button>Complete</button>
     </div>
-  )
-}
+  );
+};
 
-export default PassengerTracking
+export default PassengerTracking;
