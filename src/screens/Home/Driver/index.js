a418dbcdcf6c9c3f3e5bdb336ba2c7e0f1763fe7
ayesha-ghani098 <ayeshaghanichi@gmail.com>
2023-03-04 23:18:11 +0500

    updated firebase config

--- src/screens/Home/Driver/index.js
+++ src/screens/Home/Driver/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { ref, set,onValue } from "firebase/database";
 
 // Styles
 import styles from "./driver.module.css";
@@ -11,6 +12,7 @@ import Layout from "../../../components/Container";
 import { Heading, Text } from "../../../components/Text";
 
 import { mockDataRide } from "../../../utils/data";
+import { db } from "../../../firebase/firebaseIns";
 
 const DriverHome = () => {
   const navigate = useNavigate();
@@ -19,9 +21,44 @@ const DriverHome = () => {
     navigate("/driver/add-ride");
   };
 
+  // FIREBASE ADD DATA REMOVE IT LATER
+  const sendMessage = (message, id) => {
+    set(ref(db, "messages/" + id), {
+      message: message,
+    })
+      .then(() => {
+        console.log("successfully done");
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
+
+   // FIREBASE READ DATA REMOVE IT LATER
+   const getMessages = () => {
+
+    const messagesRef = ref(db, 'messages');
+
+    // Fetch the data
+    onValue(messagesRef, (snapshot) => {
+      const messageList = [];
+      snapshot.forEach((childSnapshot) => {
+        const childData = childSnapshot.val();
+        messageList.push(childData);
+      });
+      console.log(messageList)
+    });
+  };
+
+  let i = 1;
+
   return (
     <>
       <Layout>
+      <button onClick={getMessages}>Get Message (see in console)</button>
+        <button onClick={() => sendMessage("hello", i++)}>
+          send msg to firebase
+        </button>
         <div className={styles.addRide}>
           <Text text="Get started with just one click add a ride now!" />
         </div>
