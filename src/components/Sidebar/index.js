fd567bc86a039455989d7f0931d6fe976d19fb31
ayesha-ghani098 <ayeshaghanichi@gmail.com>
2023-03-11 03:08:56 +0500

    added data in sidebar

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Offcanvas } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 // Style and Assets
 import styles from "./Sidebar.module.css";
@@ -11,13 +12,26 @@ import Home from "../../assets/home.png";
 import Avatar from "../../assets/avatar.png";
 
 const Sidebar = ({ show, handleClose, type }) => {
+  const web3 = useSelector((state) => state.web3);
+
+  console.log("web2333", web3);
+
   return (
     <Offcanvas className={styles.sidebar} show={show} onHide={handleClose}>
       <div className={styles.header}>
         <img src={Avatar} alt="user avatar" />
         <div>
-          <h5>Ayesha Ghani</h5>
-          <p>ayeshaghanichi@Gmail.com</p>
+          {type === "driver" ? (
+            <>
+              <h5>{web3?.user?.driverinfo?.name}</h5>
+              <p>{web3?.user?.driverinfo?.email}</p>
+            </>
+          ) : (
+            <>
+              <h5>{web3?.user?.riderinfo?.name}</h5>
+              <p>{web3?.user?.riderinfo?.email}</p>
+            </>
+          )}
         </div>
       </div>
       <Offcanvas.Body>
@@ -27,10 +41,10 @@ const Sidebar = ({ show, handleClose, type }) => {
               <img src={Home} alt="icon" />
               Home
             </Link>
-            <Link className={styles.link} to="profile">
+            {/* <Link className={styles.link} to="profile">
               <img src={User} alt="icon" />
               My Profile
-            </Link>
+            </Link> */}
             <Link className={styles.link} to="/driver/my-rides">
               <img src={List} alt="icon" />
               My Rides
@@ -46,10 +60,10 @@ const Sidebar = ({ show, handleClose, type }) => {
               <img src={Home} alt="icon" />
               Home
             </Link>
-            <Link className={styles.link} to="profile">
+            {/* <Link className={styles.link} to="profile">
               <img src={User} alt="icon" />
               My Profile
-            </Link>
+            </Link> */}
             <Link className={styles.link} to="/passenger/my-rides">
               <img src={List} alt="icon" />
               My Rides
--- src/redux/web3/actions.js
+++ src/redux/web3/actions.js
@@ -30,7 +30,7 @@ const connectSuccess = (payload) => {
       dispatch(connectRequest());
    try{
     const walletAddress= await web3.eth.requestAccounts();
-    const RScontract=new web3.eth.Contract(ridesharingAbi.abi,'0xa82fF9aFd8f496c3d6ac40E2a0F282E47488CFc9');
+    const RScontract=new web3.eth.Contract(ridesharingAbi.abi,'0x5FbDB2315678afecb367f032d93F642f64180aa3');
     console.log(walletAddress,RScontract)
     let wallet={address: walletAddress[0]}
     let Ridercontract=new web3.eth.Contract(riderAbi,'0xEbBeBB565692c8A1F096643c9bc1cDf390Aebb1D');
