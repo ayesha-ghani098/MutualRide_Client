375142796bf978ff8dcf994ba0de4415241c0601
ayesha-ghani098 <ayeshaghanichi@gmail.com>
2023-03-10 22:21:18 +0500

    whatsapp work done

--- src/components/Cards/RequestCard/index.js
+++ src/components/Cards/RequestCard/index.js
@@ -1,7 +1,10 @@
-import React, { useState,useEffect } from "react";
-import { ref, set,onValue,update } from "firebase/database";
-import { db } from "../../../firebase/firebaseIns";
+import React, { useState } from "react";
+import { useSelector } from "react-redux";
 import { createSearchParams, useNavigate } from "react-router-dom";
+
+// Firebase
+import { ref, set, update, child, get, getDatabase } from "firebase/database";
+import { db } from "../../../firebase/firebaseIns";
 
 // Styles and Assets
 import styles from "../RideCard.module.css";
@@ -11,87 +14,60 @@ import Date from "../../../assets/calendar.png";
 import Money from "../../../assets/money.png";
 import Source from "../../../assets/source.png";
 import Destination from "../../../assets/destination.png";
-import Message from "../../../assets/message.png";
 import Whatsapp from "../../../assets/whatsapp.png";
+
+// Utils
 import { getLocation } from "../../../utils/geoLocation";
 
 // Components
 import { TextButton } from "../../Buttons";
-import { useDispatch, useSelector } from "react-redux";
 
 const RideCard = (props) => {
-
- 
-
   const {
-    id,
     name,
     image,
-    sourceLat,
-    sourceLong,
-    destLong,
-    destLat,
     startTime,
-    costPerKm,
     source,
     destination,
     date,
     fare,
-    rideId
+    rideId,
+    address,
   } = props.request;
+
   const navigate = useNavigate();
 
-  const dispatch = useDispatch();
   const web3 = useSelector((state) => state.web3);
-  const [mylocation,setMylocation] = useState({});
-  console.log(props.request,web3)
+  const [mylocation, setMylocation] = useState({});
+  console.log(props.request, web3);
 
   const handleJoin = async (id) => {
     const a = await getLocation(setMylocation);
     await getLocation(setMylocation);
-   console.log(mylocation)
-    const tx = await web3.rideSharingContractObj.methods.joinRide(id,web3.user.riderinfo.id).send({from:web3.wallet.address})
+    console.log(mylocation);
+    const tx = await web3.rideSharingContractObj.methods
+      .joinRide(id, web3.user.riderinfo.id)
+      .send({ from: web3.wallet.address });
     console.log(tx);
     // joinRide(uint rideId, uint riderId)
     // TODO DEKHLENA JAHAN NAVIGATE KRNA HU
-   if(true) {
-    setUserLocation(web3.wallet.address);
-    setRide(id)
-    navigate({
-      pathname: "/passenger/my-rides",
-      search: createSearchParams({
-        riderId: web3.user.riderinfo.id,
-      }).toString(),
-    });}
-  };
- 
-  const getMessages =  (id) => {
-  
-    const messagesRef = ref(db, 'users/'+id);
-
-    // Fetch the data
-    onValue(messagesRef, (snapshot) => {
-      const messageList = [];
-      snapshot.forEach((childSnapshot) => {
-        const childData = childSnapshot.val();
-        messageList.push(childData);
+    if (true) {
+      setUserLocation(web3.wallet.address);
+      setRide(id);
+      navigate({
+        pathname: "/passenger/my-rides",
+        search: createSearchParams({
+          riderId: web3.user.riderinfo.id,
+        }).toString(),
       });
-      console.log(messageList[0])
-      //setWhatsAppno(messageList[0])
-      if(messageList)
-      return messageList[0]
-    });
-  };
-  const setUserLocation = async ( id) => {
-   
-
-   
-    const datax={
-      location: mylocation,
-      cell: web3.user.riderinfo.mobileNo
-      
     }
-    console.log(id,datax)
+  };
+
+  const setUserLocation = async (id) => {
+    const datax = {
+      location: mylocation,
+      cell: web3.user.riderinfo.mobileNo,
+    };
     set(ref(db, "users/" + id), datax)
       .then(() => {
         console.log("successfully done");
@@ -100,30 +76,56 @@ const RideCard = (props) => {
         console.log("err", err);
       });
   };
-  const setRide = async ( id) => {
-    const datax={
-     rider:web3.wallet.address           
-   }
-   console.log(id,datax)
-   update(ref(db, "rides/" + id), datax)
 
-     .then(() => {
-       console.log("successfully done");
-     })
-     .catch((err) => {
-       console.log("err", err);
-     });
- };
- const handleClickWhatsApp = async () => {
-  const mobileNumber =  await getMessages("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");
-  // Replace with the desired mobile number
-  const isMobile = /iPhone|iPad|iPod|Android/i.test(window.navigator.userAgent);
-  const url = isMobile
-    ? `whatsapp://send?phone=${mobileNumber}`
-    : `https://web.whatsapp.com/send?phone=${mobileNumber}`;
-  window.open(url);
-};
+  const setRide = async (id) => {
+    const datax = {
+      rider: web3.wallet.address,
+    };
+    update(ref(db, "rides/" + id), datax)
+      .then(() => {
+        console.log("successfully done");
+      })
+      .catch((err) => {
+        console.log("err", err);
+      });
+  };
 
+  const getPhoneNumber = (id) => {
+    const dbRef = ref(getDatabase());
+    return get(child(dbRef, `users/${id}`))
+      .then((snapshot) => {
+        if (snapshot.exists()) {
+          return snapshot.val().cell;
+        } else {
+          throw new Error("No data available");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        throw error;
+      });
+  };
+
+  const handleClickWhatsApp = async () => {
+    let id = address;
+    try {
+      const phoneNumber = await getPhoneNumber(id);
+      console.log(phoneNumber);
+      if (phoneNumber) {
+        const isMobile = /iPhone|iPad|iPod|Android/i.test(
+          window.navigator.userAgent
+        );
+        const url = isMobile
+          ? `whatsapp://send?phone=${phoneNumber}`
+          : `https://wa.me/${phoneNumber}`;
+        window.open(url);
+      } else {
+        console.log("No phone number found");
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <div className={styles.card}>
@@ -136,13 +138,19 @@ const RideCard = (props) => {
         <div className={styles.info}>
           <h5>{name}</h5>
           <div>
-          <img className={styles.whatsapp} onClick={handleClickWhatsApp} src={Whatsapp} alt="icon" />
-          <img  className={styles.message} src={Message} alt="icon" /></div>
+            <img
+              className={styles.whatsapp}
+              onClick={handleClickWhatsApp}
+              src={Whatsapp}
+              alt="icon"
+            />
+          </div>
         </div>
 
         <div className={styles.location}>
           <p>
-            <img src={Source} alt="icon" />{source}
+            <img src={Source} alt="icon" />
+            {source}
           </p>
           <p>
             <img src={Destination} alt="icon" />
@@ -154,14 +162,15 @@ const RideCard = (props) => {
             <img src={Date} alt="icon" /> {date}
           </div>
           <div>
-            <img src={Time} alt="icon" />{startTime}
+            <img src={Time} alt="icon" />
+            {startTime}
           </div>
           <div>
             <img src={Money} alt="icon" /> {fare}
           </div>
         </div>
         <div className={styles.buttonContainer}>
-          <TextButton text="Join the ride" onClick={()=>handleJoin(rideId)} />
+          <TextButton text="Join the ride" onClick={() => handleJoin(rideId)} />
         </div>
       </div>
     </div>
